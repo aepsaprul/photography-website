92ca1dc1b48855c37927d105593e35c75d92f5ae
Aep <aepsaprul18@gmail.com>
2020-07-15 11:32:06 +0700

    tampilan masing2 menu

---
+++ adm/resources/views/galeri/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+
+<!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Galeri</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Galeri</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <h1 class="text-center">-- Galeri --</h1>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+
+@endsection
+
+@section('script')
+
+@endsection
--- adm/routes/web.php
+++ adm/routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/galeri', 'GaleriController@index')->name('galeri');
+Route::get('/kategori', 'KategoriController@index')->name('kategori');
+Route::get('/kontak', 'KontakController@index')->name('kontak');
+Route::get('/tentang', 'TentangController@index')->name('tentang');
